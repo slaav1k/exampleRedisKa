c5bc3e41b5b2f7802a2537aa767d4af9f3173482
Vyacheslav <slawa.121@yandex.ru>
2024-10-13 15:20:09 +0300

    Была попытка PostgresSQL + Redis. Скорость оставалась той же.

--- src/main/java/science/examplerediska/ExampleRedisKaApplication.java
+++ src/main/java/science/examplerediska/ExampleRedisKaApplication.java
@@ -2,8 +2,10 @@ package science.examplerediska;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ExampleRedisKaApplication {
 
     public static void main(String[] args) {
